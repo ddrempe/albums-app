e72ada6cc7ea24da9c3e3a9634c9e676d56550ed
Damir Drempetic <damir.drempetic@afinitos.hr>
2021-01-23 21:44:54 +0100

    Implemented query param for limit

--- src/views/Albums.jsx
+++ src/views/Albums.jsx
@@ -3,8 +3,14 @@ import { apiGetAlbums, apiGetArtists } from "dataLayer/apiClient";
 import Division from "components/atoms/Division/Division";
 import AlbumRow from "components/molecules/AlbumRow/AlbumRow";
 import { getArtistTitleById } from "dataLayer/helper";
+import { useLocation } from "react-router-dom";
+import { parseUrl } from "query-string";
 
 export default function Albums(props) {
+  const location = useLocation();
+  const limitParam = parseUrl(location.search).query.limit;
+  const limit = limitParam ? limitParam : 10;
+
   const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
