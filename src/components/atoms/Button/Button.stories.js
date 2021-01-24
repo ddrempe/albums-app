1f53aa63db38e1dd287878054c42576280960ffb
Damir Drempetic <damir.drempetic@afinitos.hr>
2021-01-24 18:23:26 +0100

    Added storybook for component documentation and showcase

---
+++ src/components/atoms/Button/Button.stories.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Button from "./Button";
+
+export default {
+  title: "Atoms/Button",
+  component: Button,
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  value: "Click",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  value: "Click",
+  secondary: true,
+};
