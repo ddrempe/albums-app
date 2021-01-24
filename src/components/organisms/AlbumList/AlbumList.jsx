e5e93e63cd6debc25a052e85f9bf7fae7f8cab9f
Damir Drempetic <damir.drempetic@afinitos.hr>
2021-01-24 18:02:37 +0100

    Added message when there is no albums

--- src/components/organisms/AlbumList/AlbumList.jsx
+++ src/components/organisms/AlbumList/AlbumList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ViewContainer from "components/molecules/ViewContainer/ViewContainer";
 import AlbumRow from "components/molecules/AlbumRow/AlbumRow";
+import Division from "components/atoms/Division/Division";
 
 export default function AlbumList(props) {
   const { onClickMarkFavorite, albums } = props;
@@ -15,6 +16,9 @@ export default function AlbumList(props) {
             onClickMarkFavorite={() => onClickMarkFavorite(item)}
           />
         ))}
+      {albums && !albums.length && (
+        <Division>There are no albums to show.</Division>
+      )}
     </ViewContainer>
   );
 }
